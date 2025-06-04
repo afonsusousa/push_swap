5d0226946b0b07e849fb2a73871638674606a8cf
afonsusousa <afonso.rocha@outlook.com>
2025-06-04 17:57:54 +0100

    removed printing operations

--- src/checker/utils/parse_operations.c
+++ src/checker/utils/parse_operations.c
@@ -6,7 +6,7 @@
 /*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 19:18:45 by amagno-r          #+#    #+#             */
-/*   Updated: 2025/06/03 20:05:36 by amagno-r         ###   ########.fr       */
+/*   Updated: 2025/06/04 17:57:09 by amagno-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@
 int exec_op(char *op, t_data *stacks)
 {
     if(!ft_strncmp(op, "sa\n", 3))
-        return (sa(stacks, false), 1);
+        return (sa(stacks, true), 1);
     if(!ft_strncmp(op, "sb\n", 3))
-        return (sb(stacks, false), 1);
+        return (sb(stacks, true), 1);
     if(!ft_strncmp(op, "ss\n", 3))
-        return (ss(stacks, false), 1);
+        return (ss(stacks, true), 1);
     if(!ft_strncmp(op, "ra\n", 3))
-        return (ra(stacks, false), 1);
+        return (ra(stacks, true), 1);
     if(!ft_strncmp(op, "rb\n", 3))
-        return (rb(stacks, false), 1);
+        return (rb(stacks, true), 1);
     if(!ft_strncmp(op, "rr\n", 3))
-        return (rr(stacks, false), 1);
+        return (rr(stacks, true), 1);
     if(!ft_strncmp(op, "rra\n", 3))
-        return (rra(stacks, false), 1);
+        return (rra(stacks, true), 1);
     if(!ft_strncmp(op, "rrb\n", 3))
-        return (rrb(stacks, false), 1);
+        return (rrb(stacks, true), 1);
     if(!ft_strncmp(op, "rrr\n", 3))
-        return (rrr(stacks, false), 1);
+        return (rrr(stacks, true), 1);
     if(!ft_strncmp(op, "pa\n", 3))
-        return (pa(stacks, false), 1);
+        return (pa(stacks, true), 1);
     if(!ft_strncmp(op, "pb\n", 3))
-        return (pb(stacks, false), 1);
+        return (pb(stacks, true), 1);
     return (0);
 }
 
