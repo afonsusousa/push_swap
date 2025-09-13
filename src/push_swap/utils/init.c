fb35beb1cfc4e15c2ee9cd1473dd093334c6c483
afonsusousa <afonso.rocha@outlook.com>
2025-09-13 20:34:56 +0100

    removed useful

--- src/push_swap/utils/init.c
+++ src/push_swap/utils/init.c
@@ -6,53 +6,13 @@
 /*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 18:29:11 by amagno-r          #+#    #+#             */
-/*   Updated: 2025/06/02 22:20:21 by amagno-r         ###   ########.fr       */
+/*   Updated: 2025/09/13 20:24:55 by amagno-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 #include <stdlib.h>
 #include <stdbool.h>
-
-static void	calculate_weights(t_stack_node *a, int total,
-	int *high_weight, int *low_weight)
-{
-	int	pos;
-	int	weight;
-
-	pos = 0;
-	while (a)
-	{
-		if (!a->in_lis)
-		{
-			weight = total - pos;
-			if (a->finalpos >= total * 2 / 3)
-				*high_weight += weight;
-			else if (a->finalpos < total / 3)
-				*low_weight += weight;
-		}
-		a = a->next;
-		pos++;
-	}
-}
-
-static int	optimal_threshold(t_data *stacks)
-{
-	int	high_weight;
-	int	low_weight;
-	int	total;
-	int	balance;
-
-	high_weight = 0;
-	low_weight = 0;
-	total = stacks->size_a;
-	calculate_weights(stacks->a, total, &high_weight, &low_weight);
-	balance = (high_weight - low_weight) * 10 / (high_weight + low_weight + 1);
-	if (balance > 0)
-		return (total * 45 / 100);
-	else
-		return (total * 55 / 100);
-}
 
 static int	update_lis(t_data *stacks)
 {
@@ -89,7 +49,7 @@ void	init_b(t_data *stacks)
 	int				threshold;
 
 	lis_size = update_lis(stacks);
-	threshold = optimal_threshold(stacks);
+	threshold = stacks->size_a / 2;
 	while (stacks->size_a > lis_size)
 	{
 		src = stacks->a;
